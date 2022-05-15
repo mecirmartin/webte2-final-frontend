87dd7090eae94525703200a58453a13f61335a25
mecirmartin <mecir.martin@gmail.com>
2022-05-15 11:55:00 +0200

    feat: Add docker and translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import { useTranslation } from "react-i18next";
+
+const App = () => {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{t("welcome.message")}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,9 +21,17 @@ function App() {
         >
           Learn React
         </a>
+        <button
+          onClick={() => {
+            console.log("change");
+            i18n.changeLanguage("sk");
+          }}
+        >
+          Change language
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import resources from "./lang.json";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "en",
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
