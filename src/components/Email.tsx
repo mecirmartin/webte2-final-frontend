ffde7b4872c62acfab4002229674197d93f9a90f
SamuelVirag <xvirags@stuba.sk>
2022-05-24 14:17:55 +0200

    feat: send email with csv

---
+++ src/components/Email.tsx
@@ -0,0 +1,36 @@
+import {Alert, AlertTitle, Backdrop, Button} from "@mui/material";
+import {useState} from "react";
+import {useTranslation} from "react-i18next";
+import {sendEmail} from "../helper/dataChanges";
+
+const AppEmail = () => {
+  const {t} = useTranslation();
+
+  const [open, setOpen] = useState<Boolean>(false);
+
+  const handleClick = () => {
+    sendEmail();
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return open ? (
+    <Backdrop
+      sx={{color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1}}
+      open={true}
+      onClick={handleClose}
+    >
+      <Alert severity="success">
+        <AlertTitle>{t("email.successTitle.message")}</AlertTitle>
+        {t("email.success.message")}
+      </Alert>
+    </Backdrop>
+  ) : (
+    <Button onClick={handleClick}>{t("email.send.message")}</Button>
+  );
+};
+
+export default AppEmail;
